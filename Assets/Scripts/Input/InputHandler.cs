0464376ed86934642f5c1e2c42525f3bbf95a7ed
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-10-02 16:42:04 -0400

    planning out input handler and character engine
    working to get movement working first

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -1,3 +1,5 @@
+/* Code by: Matthew Sheehan */
+
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,7 +8,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
 
-
+/// <summary>
+/// CrashLanding v03 
+/// </summary>
 namespace CL03
 {
     //This needs to go first
@@ -17,7 +21,93 @@ namespace CL03
     /// </summary>
     public class InputHandler : MonoBehaviour
     {
+        #region Move Events and Values
+        [Header("Move Events")]
+        public bool crouchPressed;
+        public bool crouchHeld;
+        public bool jumpPressed;
+        public bool jumpHeld;
+        [Header("Move Value")]
+        public float horizontal;
+        public float vertical;
+        #endregion
 
+        [Space]
 
+        #region Action Events
+        [Header("Action Events")]
+        public bool interactPressed;
+        public bool interactHeld;
+        public bool dropObjectPressed;
+        public bool dropObjectHeld;
+        public bool changeObjectPressed;
+        public bool changeObjectHeld;
+
+        public bool consoleLaunchHeld;
+        public bool consoleLaunchPressed;
+
+        public bool action1Pressed;
+        public bool action1Held;
+        public bool action2Pressed;
+        public bool action2Held;
+        #endregion
+
+        #region Character Change Events
+        [Header("Character Change Events")]
+        public int lastCharacterCalled;
+        public bool forwardChange;
+        public bool backwardChange;
+        public bool chooseChar1;
+        public bool chooseChar2;
+        public bool chooseChar3;
+        public bool chooseChar4;
+        public bool chooseChar5;
+        #endregion
+
+        #region Private Fields
+        private Vector2 tempMovement;
+        private PlayerControls controls;
+        bool readyToClear;                              //Bool used to keep input in sync
+        #endregion
+
+        //movement events
+        #region BASIC MOVEMENT
+
+        /// <summary>
+        /// Directional Movement: WASD keys are hit; Vector2
+        /// </summary>
+        /// <param name="context">Vector 2</param>
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            tempMovement = context.ReadValue<Vector2>();
+            horizontal = tempMovement.x;
+            vertical = tempMovement.y;
+        }
+
+        /// <summary>
+        /// Control Key
+        /// </summary>
+        /// <param name="context">Button</param>
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            if (context.interaction is HoldInteraction)
+                crouchHeld = context.ReadValueAsButton();
+            else
+                crouchPressed = context.ReadValueAsButton();
+        }
+
+        /// <summary>
+        /// Space Bar Key
+        /// </summary>
+        /// <param name="context">Button</param>
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            //           jumpPressed = jumpHeld || Keyboard.current.spaceKey.wasPressedThisFrame;
+            if (context.interaction is HoldInteraction)
+                jumpHeld = context.ReadValueAsButton();
+            else
+                jumpPressed = context.ReadValueAsButton();
+        }
+        #endregion
     }
 }
--- Assets/Scripts/Player/CharacterEngine.cs
+++ Assets/Scripts/Player/CharacterEngine.cs
@@ -1,22 +1,72 @@
+/* Code by: Matthew Sheehan */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
-public class CharacterEngine : MonoBehaviour
-{ 
-
-    #region Hanging Properties
-
-    #endregion
-    // Start is called before the first frame update
-    void Start()
+/// <summary>
+/// CrashLanding v03 
+/// </summary>
+namespace CL03
+{
+    /// <summary>
+    /// Character script, Handles individual character's control,
+    /// states, raycasts, actions and physics.
+    /// </summary>
+    public class CharacterEngine : MonoBehaviour
     {
-        
-    }
+		#region Component References: Rigidbody, BoxCollider, Input Handler
+		BoxCollider2D bodyCollider;             //The collider component
+		Rigidbody2D rigidBody;                  //The rigidbody component
+		InputHandler input;                     //The current inputs for the player
+		#endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		#region Character States
+
+
+		public bool canHang = true; // can your character even hang dawg?
+		public float cantHangCoolDownTime = 1.5f;
+		#endregion
+
+		#region Basic Horizontal Movement
+		void GroundMovement()
+		{
+			//If currently hanging, the player can't move to exit
+			if (isHanging)
+				return;
+
+			//Handle crouching input. If holding the crouch button but not crouching, crouch
+			if (input.crouchHeld && !isCrouching && isOnGround)
+				Crouch();
+			//Otherwise, if not holding crouch but currently crouching, stand up
+			else if (!input.crouchHeld && isCrouching)
+				StandUp();
+			//Otherwise, if crouching and no longer on the ground, stand up
+			else if (!isOnGround && isCrouching)
+				StandUp();
+
+			//Calculate the desired velocity based on inputs
+			float xVelocity = speed * input.horizontal;
+
+			//If the sign of the velocity and direction don't match, flip the character
+			if (xVelocity * direction < 0f)
+				FlipCharacterDirection();
+
+			//If the player is crouching, reduce the velocity
+			if (isCrouching)
+				xVelocity /= crouchSpeedDivisor;
+
+			//Apply the desired velocity 
+			rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
+
+			//If the player is on the ground, extend the coyote time window
+			if (isOnGround)
+				coyoteTime = Time.time + coyoteDuration;
+		}
+
+		#endregion
+		
+
+	}
 }
