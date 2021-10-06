8a943221f406d62edd2fa06a1d6a2729a34e3af4
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-10-06 11:52:39 -0400

    Update InteractableObjects.cs

--- Assets/Scripts/Interactables/InteractableObjects.cs
+++ Assets/Scripts/Interactables/InteractableObjects.cs
@@ -1,21 +1,31 @@
+/* Code by: Matthew Sheehan */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace CL03
 {
-    public class InteractableObjects : MonoBehaviour
+    /// <summary>
+    /// Base class for which objects which the character interacts with
+    /// are abstracted from.
+    ///
+    /// state can be interacted with
+    /// state is being interacted with
+    /// </summary>
+    public class InteractableObjects : MonoBehaviour //ScriptableObject
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+    // an interactable doesnt always have to be interactable
+    //  public bool isInteractableState {get; protected set;}
+        public bool isInteractedWith { get; protected set; }
+        public bool canBeStored { get; protected set; }
 
-        // Update is called once per frame
-        void Update()
+        //to be overwritten
+        public virtual void Interact(CharacterEngine character)
         {
-        
+            //this script wont show unless using this class as a test.
+            Debug.Log("Object recognizes - interact test");
+            character.isInteracting_Test = true;
         }
     }
 }
