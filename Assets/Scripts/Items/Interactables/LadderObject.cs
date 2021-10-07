055b115849acff4e3208f1dcc4393af12f9e13db
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-10-07 08:40:24 -0400

    Update LadderObject.cs

--- Assets/Scripts/Items/Interactables/LadderObject.cs
+++ Assets/Scripts/Items/Interactables/LadderObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace CL03
 {
+
+    /// <summary>
+    /// Ladder objects allow the character to move vertically without jump.
+    /// </summary>
     public class LadderObject : MonoBehaviour
     {
         
